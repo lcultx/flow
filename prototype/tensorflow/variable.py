e369cf62942b28a1da5c449b0e27116cd532093c
qian <qian@localhost.localdomain>
2018-01-20 17:57:28 +0800

    python tensorflow demo

---
+++ prototype/tensorflow/variable.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+state = tf.Variable(0, name="conunter")
+print(state.name)
+
+one = tf.constant(1)
+new_value = tf.add(state ,one)
+update = tf.assign(state, new_value)
+
+# must have if define variable
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
